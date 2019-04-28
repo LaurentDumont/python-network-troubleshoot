8b89dc02a9a91c1cac90df591c7e25f2b657deef
Laurent Dumont <laurentfdumont@gmail.com>
2019-04-28 18:28:04 -0400

    Working threading and error handling

--- network-tshoot.py
+++ network-tshoot.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 import curses
 import time
 import threading
@@ -21,24 +19,28 @@ class Ping(threading.Thread):
         self.start()
 
     def ping(self):
-        while True: 
-            ping_parser = pingparsing.PingParsing()
-            transmitter = pingparsing.PingTransmitter()
-            transmitter.destination_host = self.target
-            transmitter.count = self.ping_count
-            ping_result = transmitter.ping()
-            ping_result_json = json.loads(json.dumps(ping_parser.parse(ping_result).as_dict()))
-            begin_x = 0; begin_y = 0
-            height = 10; width = 40
-            ping_win_section = curses.newwin(height, width, begin_y, begin_x)
-            ping_win_section.addstr('Ping statistics for {}: \n'.format(self.target))
-            ping_win_section.addstr('Round Trip Time Minimum is : {} ms\n'.format(str(ping_result_json['rtt_min'])))
-            ping_win_section.addstr('Round Trip Time Maximum is : {} ms\n'.format(str(ping_result_json['rtt_max'])))
-            ping_win_section.addstr('Round Trip Time Average is : {} ms\n'.format(str(ping_result_json['rtt_avg'])))
-            ping_win_section.addstr('Average packet loss : {} \n'.format(str(ping_result_json['packet_loss_rate'])))
-            ping_win_section.refresh()
-            ping_win_section.clear()
-            time.sleep(2)
+        begin_x = 0; begin_y = 0
+        height = 10; width = 50
+        ping_win_section = curses.newwin(height, width, begin_y, begin_x)
+        while True:
+            try:
+                ping_parser = pingparsing.PingParsing()
+                transmitter = pingparsing.PingTransmitter()
+                transmitter.destination_host = self.target
+                transmitter.count = self.ping_count
+                ping_result = transmitter.ping()
+                ping_result_json = json.loads(json.dumps(ping_parser.parse(ping_result).as_dict()))
+                ping_win_section.addstr('Ping statistics for {}: \n'.format(self.target))
+                ping_win_section.addstr('Round Trip Time Minimum is : {} ms\n'.format(str(ping_result_json['rtt_min'])))
+                ping_win_section.addstr('Round Trip Time Maximum is : {} ms\n'.format(str(ping_result_json['rtt_max'])))
+                ping_win_section.addstr('Round Trip Time Average is : {} ms\n'.format(str(ping_result_json['rtt_avg'])))
+                ping_win_section.addstr('Average packet loss : {} \n'.format(str(ping_result_json['packet_loss_rate'])))
+                ping_win_section.refresh()
+                ping_win_section.clear()
+                time.sleep(2)
+            except: 
+                continue
+
 
 class PublicAddress(threading.Thread):
 
@@ -50,18 +52,21 @@ class PublicAddress(threading.Thread):
         self.start()
 
     def GetPublicAddress(self):
+        begin_x = 70; begin_y = 0
+        height = 5; width = 50
+        public_ip_win_section = curses.newwin(height, width, begin_y, begin_x)
         while True:
-            begin_x = 50; begin_y = 0
-            height = 5; width = 40
-            public_ip_data = requests.get('https://api.ipify.org?format=json')
-            public_ip_json = json.loads(public_ip_data.text)
-            public_ip_only = public_ip_json['ip']
-            public_ip_win_section = curses.newwin(height, width, begin_y, begin_x)
-            public_ip_win_section.addstr('Getting public IP address : \n')
-            public_ip_win_section.addstr(public_ip_only)
-            public_ip_win_section.refresh()
-            public_ip_win_section.clear()
-            time.sleep(10)
+           try:
+               public_ip_data = requests.get('https://api.ipify.org?format=json')
+               public_ip_json = json.loads(public_ip_data.text)
+               public_ip_only = public_ip_json['ip']
+               public_ip_win_section.addstr('Getting public IP address : \n')
+               public_ip_win_section.addstr(public_ip_only)
+               public_ip_win_section.refresh()
+               public_ip_win_section.clear()
+               time.sleep(10)
+           except:
+               continue
 
 
 def run(stdscr):
@@ -81,6 +86,8 @@ def run(stdscr):
             continue
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     stdscr = curses.initscr()
     curses.wrapper(run)
+    stdscr.addstr('Something went wrong')
+    curses.endwin()
