474198ab834a5a5c62d5426544211f81b093b254
Laurent Dumont <laurentfdumont@gmail.com>
2019-05-12 16:54:16 -0400

    Scapy library module

--- cdp_client.py
+++ cdp_client.py
@@ -1,7 +1,13 @@
 from scapy.all import *
 
-
-def get_cdp_packet():
-    return
-
-    
+load_contrib('cdp')
+cdp_packet = sniff(iface='eno1', count=1, filter='ether dst 01:00:0c:cc:cc:cc', store=1)
+device_name = cdp_packet[0]["CDPMsgDeviceID"].val.decode()
+port_name = cdp_packet[0]["CDPMsgPortID"].iface.decode()
+platform_version = cdp_packet[0]["CDPMsgPlatform"].val.decode()
+software_version = cdp_packet[0]["CDPMsgSoftwareVersion"].val.decode()
+#Is 0 for half and 1 for full duplex.
+duplex_status = cdp_packet[0]["CDPMsgDuplex"].duplex
+primary_vlan = cdp_packet[0]["CDPMsgNativeVLAN"].vlan
+vlan = cdp_packet[0]["CDPMsgNativeVLAN"].vlan
+management_ip_address = cdp_packet[0]["CDPAddrRecordIPv4"].addr
